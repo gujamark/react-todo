0e59fc96f5756d3c3602ff66548a6fa2874502aa
Guja Markozashvili <gujamarkozashvili@Gujas-MacBook-Air.local>
2021-04-25 22:15:34 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import ErrorBoundary from './error-boundary';
+import TodoPage from './pages/Todopage';
+import Theme from './theme';
+import TodoProviderComponent from './contexts/TodoProvier';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+      <Theme>
+        <TodoProviderComponent>
+          <TodoPage />
+        </TodoProviderComponent>
+      </Theme>
+    </ErrorBoundary>
   );
 }
 
